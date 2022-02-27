63b357322abf402f55f1f559de07b7a1050af23e
nandinivit6 <nandinivit6@gmail.com>
2022-02-28 01:16:18 +0530

    code_added

--- array_vector_easy/richest_customer_wealth.cpp
+++ array_vector_easy/richest_customer_wealth.cpp
@@ -1,1 +1,20 @@
 https://leetcode.com/problems/richest-customer-wealth/submissions/
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) 
+    {
+        int mx=0;
+        int sum=0;
+        
+        for(int i=0;i<accounts.size();i++)
+        {
+            for(int j=0;j<accounts[i].size();j++)
+            {
+                sum+=accounts[i][j];
+            }
+            mx=max(mx,sum);
+            sum=0;
+        }
+        return mx;
+    }
+};
