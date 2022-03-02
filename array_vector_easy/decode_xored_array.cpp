f95aa567129db6dbeb25eecab704accf58590b29
nandinivit6 <nandinivit6@gmail.com>
2022-03-02 11:32:18 +0530

    2/3/22

---
+++ array_vector_easy/decode_xored_array.cpp
@@ -0,0 +1,15 @@
+https://leetcode.com/problems/decode-xored-array/
+class Solution {
+public:
+    vector<int> decode(vector<int>& encoded, int first) 
+    {
+        vector<int> arr;
+        arr.push_back(first);
+        
+        for(int i=0;i<encoded.size();i++)
+        {
+            arr.push_back(arr[i]^encoded[i]);
+        }
+        return arr;
+    }
+};
---
+++ array_vector_easy/no_smaller_than_current.cpp
@@ -0,0 +1,23 @@
+https://leetcode.com/problems/how-many-numbers-are-smaller-than-the-current-number/
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) 
+    {
+        vector<int> v;
+        int count=0;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            count=0;
+            for(int j=0;j<nums.size();j++)
+            {
+                if(nums[j]<nums[i])
+                {
+                    count++;
+                }
+            }
+            v.push_back(count);
+        }
+        return v;      
+    }
+};
